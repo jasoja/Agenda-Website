8bdf231e136ee982a2f5beda1cba522fadf6bb09
jtj54 <jtj54@drexel.edu>
2023-02-24 16:08:13 -0500

    remade checklist.html

--- vision_app/views.py
+++ vision_app/views.py
@@ -16,10 +16,6 @@ def about():
 def checklist():
     return render_template("checklist.html")
 
-@app.route("/index/")
-def index():
-    return render_template("index.html")
-
 @app.route("/login/")
 def login():
     return render_template("login.html")
