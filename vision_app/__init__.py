9f64e97717da2fc2d4831c1fc674d918865a5922
bql23 <bql23@drexel.edu>
2023-03-03 09:17:10 -0500

    branch database

--- vision_app/__init__.py
+++ vision_app/__init__.py
@@ -1,2 +1,17 @@
-import flask
-app = flask.Flask(__name__)
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__, instance_relative_config=True)
+
+app.app_context().push()
+
+app.config["SECRET_KEY"] = '48391ndquhnf'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///user_information.db'
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+app.config.from_object('config')
+
+from . import views
+from . import models
---
+++ vision_app/models.py
@@ -0,0 +1,22 @@
+from vision_app import db
+from werkzeug.security import check_password_hash, generate_password_hash
+from datetime import datetime 
+import sqlalchemy
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), index=True, unique = True, nullable=False)
+    password_hash = db.Column(db.String(128))
+    items = db.relationship('Item', backref='user',lazy=True)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Item(db.Model):
+    user_name = db.Column(db.String, db.ForeignKey('user.name')) # access the user's unique name
+    task = db.Column(db.String(128), primary_key=True) # task list name
+    date = db.Column(db.DateTime(timezone=True), default=datetime.now) # the date
+ 
