83b75f69740a15fab4ddb2652a41383e14c0a50c
bql23 <bql23@drexel.edu>
2023-03-20 22:42:57 -0400

    deleted unecessary comments

--- vision_app/views.py
+++ vision_app/views.py
@@ -35,42 +35,42 @@ def login():
 def register():
     return render_template("register.html")
 
-@app.route('/register/register-user', methods=['POST'])
-def register_user():
-    form = request.form
-    user = User(
-        email = form['email-address'],
-        name = form['name']
-    )
-    user.set_password(form['password'])
-    db.session.add(user)   
-    db.session.commit()
-    flash("Successfully registered, please login.")
-    return redirect(url_for('home')) 
+# @app.route('/register/register-user', methods=['POST'])
+# def register_user():
+#     form = request.form
+#     user = User(
+#         email = form['email-address'],
+#         name = form['name']
+#     )
+#     user.set_password(form['password'])
+#     db.session.add(user)   
+#     db.session.commit()
+#     flash("Successfully registered, please login.")
+#     return redirect(url_for('home')) 
 
-@app.route('/validate-user-registration', methods=['POST'])
-def validate_user_registration():
-    if request.method == "POST":
-        email_address = request.get_json()['email'] 
-        user = User.query.filter_by(email=email_address).first()
-        if user:
-            return jsonify({"user_exists": "true"})
-        else:
-            return jsonify({"user_exists": "false"})
+# @app.route('/validate-user-registration', methods=['POST'])
+# def validate_user_registration():
+#     if request.method == "POST":
+#         email_address = request.get_json()['email'] 
+#         user = User.query.filter_by(email=email_address).first()
+#         if user:
+#             return jsonify({"user_exists": "true"})
+#         else:
+#             return jsonify({"user_exists": "false"})
 
-@app.route('/home/login-user', methods=['POST'])
-def login_user():
-    form = request.form
-    user = User.query.filter_by(email = form['email-address']).first()
-    if not user:
-        flash("User doesn't exist")
-        return redirect(url_for('home')) 
-    if user.check_password(form['password']):
-        session['user'] = user.id 
-        return redirect(url_for('calendar')) 
-    else:  
-        flash('Password was incorrect')
-        return redirect(url_for('home')) 
+# @app.route('/home/login-user', methods=['POST'])
+# def login_user():
+#     form = request.form
+#     user = User.query.filter_by(email = form['email-address']).first()
+#     if not user:
+#         flash("User doesn't exist")
+#         return redirect(url_for('home')) 
+#     if user.check_password(form['password']):
+#         session['user'] = user.id 
+#         return redirect(url_for('calendar')) 
+#     else:  
+#         flash('Password was incorrect')
+#         return redirect(url_for('home')) 
 
 @app.route('/home/logout-user', methods=['POST','GET'])
 def logout_user():
